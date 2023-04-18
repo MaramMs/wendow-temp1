18e02355d9ac3d87b8232d13bc316ab8d9a4f3f3
Maran.I.Msabeh <maramismail2015@gmail.com>
2023-04-18 16:07:40 +0300

    whitelist mobile

--- pages/whitelist.js
+++ pages/whitelist.js
@@ -1,63 +1,77 @@
-import Wrapper from '@/components/Wrapper'
-import React from 'react'
+import Wrapper from "@/components/Wrapper";
+import React from "react";
 
-import { Space, Table, Tag } from 'antd';
-import { CloseCircleOutlined } from '@ant-design/icons';
+import { Space, Table, Tag } from "antd";
+import { CloseCircleOutlined } from "@ant-design/icons";
 const columns = [
   {
-    title: 'The product name',
-    dataIndex: 'name',
-    key: 'name',
+    title: "The product name",
+    dataIndex: "name",
+    key: "name",
     render: (text, record) => (
-        <div className='flex gap-2 items-center'>
-          <span><CloseCircleOutlined /></span>
-          <img src={record.image} alt={record.name} width={50} />
-        </div>
-      ),
+      <div className="flex gap-2 items-center">
+        <span>
+          <CloseCircleOutlined />
+        </span>
+        <img src={record.image} alt={record.name} width={50} />
+      </div>
+    ),
   },
   {
-    title: 'The price',
-    dataIndex: 'price',
-    key: 'price',
+    title: "The price",
+    dataIndex: "price",
+    key: "price",
   },
   {
-    title: 'a viable in the store',
-    dataIndex: 'store',
-    key: 'store',
-    className:'text-[#67C6B0]'
-
+    title: "a viable in the store",
+    dataIndex: "store",
+    key: "store",
+    className: "text-[#67C6B0]",
   },
 ];
 const data = [
   {
-    key: '1',
-    image:'/images/products/cart-product.png',
+    key: "1",
+    image: "/images/products/cart-product.png",
     price: 32,
-    store: 'a viable',
+    store: "a viable",
   },
   {
-    key: '2',
-    image:'/images/products/cart-product.png',
+    key: "2",
+    image: "/images/products/cart-product.png",
     price: 32,
-    store: 'not a viable',
+    store: "not a viable",
   },
   {
-    key: '3',
-    image:'/images/products/cart-product.png',
+    key: "3",
+    image: "/images/products/cart-product.png",
     price: 32,
-    store: 'not a viable',
+    store: "not a viable",
   },
 ];
 const whitelist = () => {
   return (
     <>
-    <div className='h-[239px] bg-[#FAFAFA] flex justify-center items-center'><h1 className='text-center 	 text-[24px] font-semibold'>The Favorite</h1></div>
-    <Wrapper >
-    <Table bordered={false}columns={columns} dataSource={data}  style={{borderTop:'1px solid #EDEDED' ,borderBottom:'1px solid #EDEDED'}} className='list-table mb-[500px]' pagination={false}/>
-    </Wrapper>
-      
+      <div
+        className="h-[239px] bg-[#FAFAFA] flex justify-center items-center max-[575px]:h-[110px] max-[575px]:bg-gradient-to-br from-purple-800 to-teal-300"
+      >
+        <h1 className="text-center 	 text-[24px] font-semibold max-[575px]:text-white">The Favorite</h1>
+      </div>
+      <Wrapper>
+        <Table
+          bordered={false}
+          columns={columns}
+          dataSource={data}
+          style={{
+            borderTop: "1px solid #EDEDED",
+            borderBottom: "1px solid #EDEDED",
+          }}
+          className="list-table mb-[500px] max-[575px]:px-5 "
+          pagination={false}
+        />
+      </Wrapper>
     </>
-  )
-}
+  );
+};
 
-export default whitelist
+export default whitelist;
