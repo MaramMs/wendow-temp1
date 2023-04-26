ee2e7544dce991957a893ccc8536e979c3cbc824
Maran.I.Msabeh <maramismail2015@gmail.com>
2023-04-26 19:24:45 +0300

    fix some issue in mobile cart

--- components/MobileCart.js
+++ components/MobileCart.js
@@ -9,9 +9,9 @@ const MobileCart = () => {
     <CloseOutlined className='absolute left-[15px] top-[15px] text-[#7251A2]' />
      <div className='card flex'>
         <img src='/images/products/cart-product.png'/>
-     <div className='flex flex-col gap-[19px]' >
+     <div className='flex flex-col gap-[19px] grow' >
         <h1 className='text-[#ACACAC] text-[12px] font-medium'>the product name</h1>
-        <div className="border border-[#E8E8E8] w-[101px] h-[38px] flex justify-between items-center px-5">
+        <div className="border border-[#E8E8E8] w-[101px] h-[38px] flex justify-between items-center px-5 mr-auto">
            <span className="cursor-pointer">+</span>
            <span className="">1</span>
            <span className="cursor-pointer">-</span>
@@ -22,14 +22,14 @@ const MobileCart = () => {
 
      </div>
     </Col>
-
-    <Col span={24} className=' h-[140px]  mb-[15px] bg-[#FAFAFA] px-[19px] py-[20px] relative'>
-    <CloseOutlined className='absolute left-0 top-0 text-[#7251A2] left-[15px] top-[15px]' />
+    
+    <Col span={24 } className=' h-[140px] mb-[15px] bg-[#FAFAFA] px-[19px] py-[20px] relative'>
+    <CloseOutlined className='absolute left-[15px] top-[15px] text-[#7251A2]' />
      <div className='card flex'>
         <img src='/images/products/cart-product.png'/>
-     <div className='flex flex-col gap-[19px]' >
+     <div className='flex flex-col gap-[19px] grow' >
         <h1 className='text-[#ACACAC] text-[12px] font-medium'>the product name</h1>
-        <div className="border border-[#E8E8E8] w-[101px] h-[38px] flex justify-between items-center px-5">
+        <div className="border border-[#E8E8E8] w-[101px] h-[38px] flex justify-between items-center px-5 mr-auto">
            <span className="cursor-pointer">+</span>
            <span className="">1</span>
            <span className="cursor-pointer">-</span>
@@ -41,13 +41,13 @@ const MobileCart = () => {
      </div>
     </Col>
 
-    <Col span={24} className=' h-[140px] mb-[15px] bg-[#FAFAFA] px-[19px] py-[20px] relative'>
-    <CloseOutlined className='absolute left-0 top-0 text-[#7251A2] left-[15px] top-[15px]' />
+    <Col span={24 } className=' h-[140px] mb-[15px] bg-[#FAFAFA] px-[19px] py-[20px] relative'>
+    <CloseOutlined className='absolute left-[15px] top-[15px] text-[#7251A2]' />
      <div className='card flex'>
         <img src='/images/products/cart-product.png'/>
-     <div className='flex flex-col gap-[19px]' >
+     <div className='flex flex-col gap-[19px] grow' >
         <h1 className='text-[#ACACAC] text-[12px] font-medium'>the product name</h1>
-        <div className="border border-[#E8E8E8] w-[101px] h-[38px] flex justify-between items-center px-5">
+        <div className="border border-[#E8E8E8] w-[101px] h-[38px] flex justify-between items-center px-5 mr-auto">
            <span className="cursor-pointer">+</span>
            <span className="">1</span>
            <span className="cursor-pointer">-</span>
@@ -59,13 +59,13 @@ const MobileCart = () => {
      </div>
     </Col>
 
-    <Col span={24} className=' h-[140px]  mb-[15px] bg-[#FAFAFA] px-[19px] py-[20px] relative'>
-    <CloseOutlined className='absolute left-0 top-0 text-[#7251A2] left-[15px] top-[15px]' />
+    <Col span={24 } className=' h-[140px] mb-[15px] bg-[#FAFAFA] px-[19px] py-[20px] relative'>
+    <CloseOutlined className='absolute left-[15px] top-[15px] text-[#7251A2]' />
      <div className='card flex'>
         <img src='/images/products/cart-product.png'/>
-     <div className='flex flex-col gap-[19px]' >
+     <div className='flex flex-col gap-[19px] grow' >
         <h1 className='text-[#ACACAC] text-[12px] font-medium'>the product name</h1>
-        <div className="border border-[#E8E8E8] w-[101px] h-[38px] flex justify-between items-center px-5">
+        <div className="border border-[#E8E8E8] w-[101px] h-[38px] flex justify-between items-center px-5 mr-auto">
            <span className="cursor-pointer">+</span>
            <span className="">1</span>
            <span className="cursor-pointer">-</span>
@@ -76,6 +76,25 @@ const MobileCart = () => {
 
      </div>
     </Col>
+
+    <Col span={24 } className=' h-[140px] mb-[15px] bg-[#FAFAFA] px-[19px] py-[20px] relative'>
+    <CloseOutlined className='absolute left-[15px] top-[15px] text-[#7251A2]' />
+     <div className='card flex'>
+        <img src='/images/products/cart-product.png'/>
+     <div className='flex flex-col gap-[19px] grow' >
+        <h1 className='text-[#ACACAC] text-[12px] font-medium'>the product name</h1>
+        <div className="border border-[#E8E8E8] w-[101px] h-[38px] flex justify-between items-center px-5 mr-auto">
+           <span className="cursor-pointer">+</span>
+           <span className="">1</span>
+           <span className="cursor-pointer">-</span>
+           </div>
+<span className='text-[#67C6B0] text-[12px] font-medium'> 60$</span>
+
+     </div>
+
+     </div>
+    </Col>
+
    
    </Row>
   )
