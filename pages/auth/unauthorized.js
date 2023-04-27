fcc0be181c043d02e63d9462008d06826f80e821
Maran.I.Msabeh <maramismail2015@gmail.com>
2023-04-27 14:09:04 +0300

    unauthorized mobile

--- pages/auth/unauthorized.js
+++ pages/auth/unauthorized.js
@@ -5,12 +5,12 @@ import React from 'react'
 const unauthorized = () => {
   return (
     <UnauthorizedLayout >
-    <div className='bg-white w-[690px] h-[517px] flex flex-col items-center pt-[70px]'>
-      <img src='/images/logo.png' className='w-[54px] h-[64px] mb-[20px]' />
-      <span className='text-[18px] font-bold text-[#7251A2] mb-[50px]'> Wendow App</span>
-      <h3 className='text-[#7B5DA8] font-bold text-[34px] mb-[30px]'>Coming Soon ...</h3>
-      <p className='text-[16px] font-normal text-[#1a1a1a] mb-[50px]'>The store is temporarily closed, it will be fixed shortly </p>
-      <hr className='w-[200px] h-1  bg-gradient-to-r from-purple-600 to-purple-200 mb-[30px]' />
+    <div className='bg-white md:w-[690px] md:h-[517px] flex flex-col items-center md:pt-[70px] w-[335px] h-[344px] pt-[20px] '>
+      <img src='/images/logo.png' className='w-[54px] h-[64px] md:mb-[20px]' />
+      <span className='text-[18px] font-bold text-[#7251A2] md:mb-[50px] mb-[30px]'> Wendow App</span>
+      <h3 className='text-[#7B5DA8] font-bold md:text-[34px] md:mb-[30px] text-[20px] mb-[30px]'>Coming Soon ...</h3>
+      <p className='text-[16px] font-normal text-[#1a1a1a] md:mb-[50px] mb-[30px] w-[295px] text-center md:w-[344px] '>The store is temporarily closed, it will be fixed shortly </p>
+      <hr className='w-[200px] h-1  bg-gradient-to-r from-purple-600 to-purple-200 md:mb-[30px] mb-[10px]' />
 
       <ul className='flex gap-[20px]'>
         <li>
