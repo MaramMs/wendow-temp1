8e53f8e3942659bd052611b622d1092d0c61136f
Maran.I.Msabeh <maramismail2015@gmail.com>
2023-04-27 15:48:34 +0300

    reset password page

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import Layout from '@/components/Layout'
 import '@/styles/globals.css'
 import unauthorized from './auth/unauthorized';
 export default function App({ Component, pageProps  }) {
-  const isAuthorized = false; // Replace with your authentication logic
+  const isAuthorized = true; // Replace with your authentication logic
   // console.log(!isAuthorized , Component !== unauthorized)
   if (!isAuthorized && Component !== unauthorized) {
     Component = unauthorized;
---
+++ pages/resetpassword.js
@@ -0,0 +1,92 @@
+import Wrapper from "@/components/Wrapper";
+import { Button, Checkbox, Form, Input } from "antd";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+
+const resetpassword = () => {
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+  return (
+    <Wrapper>
+      <div className="flex flex-col justify-center md:items-center  px-[16px] md:px-0">
+        <h1 className="text-[#67C6B0] text-[20px] font-medium md:text-right">
+          {" "}
+          Enter a New Password
+        </h1>
+        <p className="text-[#797979] text-[16px] font-normal">
+          Enter a strong hard-to-guess password
+        </p>
+
+        <Form
+          name="normal_login"
+          className="login-form mt-[127px] md:mt-[22px]"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="currentpassword"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+           
+          >
+            <Input.Password
+              placeholder="a current password"
+              className=" w-[359px] h-[54px]  bg-black border border-[#eee]"
+              style={{ backgroundColor: "#FAFAFA" }}
+            />
+          </Form.Item>
+
+          <Form.Item
+            name="newpassword"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password
+              placeholder="a new password"
+              className=" w-[359px] h-[54px] bg-[#FAFAFA]  border border-[#eee]"
+              style={{ backgroundColor: "#FAFAFA" }}
+            />
+          </Form.Item>
+
+          <Form.Item
+            name="confirmpassword"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password
+              placeholder="confirm a new password"
+              className=" w-[359px] h-[54px] bg-[#FAFAFA]  border border-[#eee]"
+              style={{ backgroundColor: "#FAFAFA" }}
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button bg-[#67C6B0] flex justify-center items-center m-auto w-[358px] h-[38px]"
+            >
+              Save
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default resetpassword;
