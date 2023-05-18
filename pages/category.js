7c6e6b9581248f72416bd903da0fe5d7b54da671
Maran.I.Msabeh <maramismail2015@gmail.com>
2023-05-18 15:14:17 +0300

    edit category page

--- pages/category.js
+++ pages/category.js
@@ -11,12 +11,11 @@ import { FiFilter } from "react-icons/fi";
 const category = () => {
   const { width } = useWindowSize();
   const isMobile = width <= 768;
-  const [modal ,setShowModal] = useState(false)
+  const [modal, setShowModal] = useState(false);
 
-  const showModal = () =>{
+  const showModal = () => {
     setShowModal(true);
-   
-  }
+  };
 
   console.log(modal);
   return (
@@ -39,26 +38,24 @@ const category = () => {
             sm: 8,
           }}
         >
-          <Col lg={{ span: 20 }} xxl={{ span: 20 }}>
-            <img
-              src="/images/black-lap.png"
-              className="mb-[32px] max-sm:px-[17px]"
-            />
+          <Col span={24} md={{ span: 18 }}>
             {isMobile && (
               <div className="flex justify-center items-center  mt-[33px] mb-[26px] bg-[#FBF8FF] rounded-[8px] mr-auto w-[59px] h-[58px]">
-                <FiFilter className="text-[18px] text-[#7251A2] cursor-pointer" onClick={showModal}/>
+                <FiFilter
+                  className="text-[18px] text-[#7251A2] cursor-pointer"
+                  onClick={showModal}
+                />
               </div>
+            )}
 
-            )
-            
-           
-            }
+            {modal && <FilterModal setShowModal={setShowModal} modal={modal} />}
 
-            {
-              modal && <FilterModal setShowModal={setShowModal} modal={modal}/>
-            }
+            <Row className="md:mb-[264px]">
+              <Col span={12} md={24}>
+                <img src="/images/black-lap.png" />
+              </Col>
+            </Row>
 
-  
             <Row
               gutter={{
                 lg: 24,
@@ -68,33 +65,33 @@ const category = () => {
               }}
               className="px-[15px]"
             >
-              <Col className="gutter-row" lg={{ span: 8 }} xxl={{ span: 7 }}>
+              <Col span={12} className="gutter-row" md={{ span: 8 }}>
                 <CardProduct />
               </Col>
-              <Col className="gutter-row" lg={{ span: 8 }} xxl={{ span: 7 }}>
+              <Col span={12} className="gutter-row" md={{ span: 8 }}>
                 <CardProduct />
               </Col>
-              <Col className="gutter-row" lg={{ span: 8 }} xxl={{ span: 7 }}>
-                <CardProduct />
-              </Col>
-
-              <Col className="gutter-row" lg={{ span: 8 }} xxl={{ span: 7 }}>
-                <CardProduct />
-              </Col>
-              <Col className="gutter-row" lg={{ span: 8 }} xxl={{ span: 7 }}>
-                <CardProduct />
-              </Col>
-              <Col className="gutter-row" lg={{ span: 8 }} xxl={{ span: 7 }}>
+              <Col span={12} className="gutter-row" md={{ span: 8 }}>
                 <CardProduct />
               </Col>
 
-              <Col className="gutter-row" lg={{ span: 8 }} xxl={{ span: 7 }}>
+              <Col span={12} className="gutter-row" md={{ span: 8 }}>
                 <CardProduct />
               </Col>
-              <Col className="gutter-row" lg={{ span: 8 }} xxl={{ span: 7 }}>
+              <Col span={12} className="gutter-row" md={{ span: 8 }}>
                 <CardProduct />
               </Col>
-              <Col className="gutter-row" lg={{ span: 8 }} xxl={{ span: 7 }}>
+              <Col span={12} className="gutter-row" md={{ span: 8 }}>
+                <CardProduct />
+              </Col>
+
+              <Col span={12} className="gutter-row" md={{ span: 8 }}>
+                <CardProduct />
+              </Col>
+              <Col span={12} className="gutter-row" md={{ span: 8 }}>
+                <CardProduct />
+              </Col>
+              <Col span={12} className="gutter-row" md={{ span: 8 }}>
                 <CardProduct />
               </Col>
             </Row>
@@ -102,8 +99,76 @@ const category = () => {
             <CustomPagination />
           </Col>
 
-          <Col lg={{ span: 4 }} xxl={{ span: 4 }} className="max-sm:hidden">
+          <Col md={{ span: 6 }} className="max-sm:hidden">
             <CustomeRang />
+
+            <>
+    <h3 className='mt-[27px] mb-[18px]'>Category by <span>Branding</span></h3>
+      <div className='flex flex-wrap gap-[6px]'>
+        <div className='rounded-[8px] w-[69px] h-[35px] flex justify-center items-center border border-[#67C6B0]'>
+            <img src='/images/apple.png'/>
+        </div>
+
+        <div className='rounded-[8px] w-[69px] h-[35px] flex justify-center items-center border border-[#67C6B0]'>
+            <img src='/images/apple.png'/>
+        </div>
+
+        <div className='rounded-[8px] w-[69px] h-[35px] flex justify-center items-center border border-[#67C6B0]'>
+            <img src='/images/apple.png'/>
+        </div>
+
+        <div className='rounded-[8px] w-[69px] h-[35px] flex justify-center items-center border border-[#67C6B0]'>
+            <img src='/images/apple.png'/>
+        </div>
+
+        <div className='rounded-[8px] w-[69px] h-[35px] flex justify-center items-center border border-[#67C6B0]'>
+            <img src='/images/apple.png'/>
+        </div>
+
+        <div className='rounded-[8px] w-[69px] h-[35px] flex justify-center items-center border border-[#67C6B0]'>
+            <img src='/images/apple.png'/>
+        </div>
+
+        <div className='rounded-[8px] w-[69px] h-[35px] flex justify-center items-center border border-[#67C6B0]'>
+            <img src='/images/apple.png'/>
+        </div>
+        <div className='rounded-[8px] w-[69px] h-[35px] flex justify-center items-center border border-[#67C6B0]'>
+            <img src='/images/apple.png'/>
+        </div>
+        
+
+      </div>
+    </>
+      <>
+        
+<h3 className='mt-[27px] mb-[18px]'>Category by <span>category</span></h3>
+<div className='flex flex-wrap gap-[7px] '>
+  <div className='flex border border-[#ACACAC] rounded-[8px]  h-[44px] p-[10px]'>
+<img src='/images/filter-category-im.png'/>
+<span className='text-[12px] text-[#ACACAC]'>Product Name</span>
+  </div>
+
+  <div className='flex border border-[#ACACAC] rounded-[8px] h-[44px] p-[10px]'>
+<img src='/images/filter-category-im.png'/>
+<span className='text-[12px] text-[#ACACAC]'>Product Name</span>
+  </div>
+
+  <div className='flex border border-[#ACACAC] rounded-[8px]  h-[44px] p-[10px]'>
+<img src='/images/filter-category-im.png'/>
+<span className='text-[12px] text-[#ACACAC]'>Product Name</span>
+  </div>
+
+  <div className='flex border border-[#ACACAC] rounded-[8px]  h-[44px] p-[10px]'>
+<img src='/images/filter-category-im.png'/>
+<span className='text-[12px] text-[#ACACAC]'>Product Name</span>
+  </div>
+</div>
+      </>
+
+
+      <div className="bg-gradient-to-b from-purple-600 to-teal-400 h-[796px] md:mt-[216px] flex justify-center items-center">
+ ads
+      </div>
           </Col>
         </Row>
       </Wrapper>
